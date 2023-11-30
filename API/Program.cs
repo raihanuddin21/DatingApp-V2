8b6b1bc017979b1f456d0b1309a02fec47c1a565
Md. Raihan Uddin <raihanuddin21@gmail.com>
2023-11-30 14:55:31 +0600

    SSL added

--- API/Program.cs
+++ API/Program.cs
@@ -25,7 +25,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
