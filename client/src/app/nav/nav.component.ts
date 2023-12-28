0f6377fa74ab8e4031df85db27b768bece028dad
Md. Raihan Uddin <raihanuddin21@gmail.com>
2023-12-28 23:32:00 +0600

    Router link added

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -7,25 +8,24 @@ import { AccountService } from '../_services/account.service';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-  model: any = {};  
+  model: any = {};
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router) { }
 
   ngOnInit(): void {
-    
+
   }
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);        
-      },
+      next: _ => this.router.navigateByUrl('/members'),
       error: error => console.log(error)
     })
   }
 
   logout() {
-    this.accountService.logout();    
+    this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
